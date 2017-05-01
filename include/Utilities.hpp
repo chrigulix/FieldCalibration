229e046174222d618dd8155afd9e0b4ee2f5b58b
Matthias <matthias.luethi@lhep.unibe.ch>
2017-05-01 13:57:25 +0200

    Add description of function

--- include/Utilities.hpp
+++ include/Utilities.hpp
@@ -9,4 +9,6 @@
 
 #include "Laser.hpp"
 
+// Split the laser track set into tracks that reached the expected exit point (within a configurable region) and others.
+// First entry of the return vector is tracks that reach the exit point, second is the ones that do not reach it.
 std::vector<Laser> ReachedExitPoint(const Laser&, float);
