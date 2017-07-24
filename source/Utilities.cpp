6e6fd6573b51f860d5877a51b43b1badd806069e
matthias <matthias.luethi@lhep.unibe.ch>
2017-07-24 09:25:29 +0200

    added comments and little refactoring.

--- source/Utilities.cpp
+++ source/Utilities.cpp
@@ -5,6 +5,12 @@
 #include "../include/Utilities.hpp"
 
 std::vector<Laser> ReachedExitPoint(const Laser& LaserSet, float ExitBoundary) {
+    /*
+     * Function that splits the input data set into two sets. The first set contains the
+     * tracks that have a distance between the last reconstructed point and the expected
+     * exit point smaller than the specified distance (ExitBoundary). All other LaserTracks
+     * will be put in the second set.
+     */
 
     std::vector<Laser> Selection;
     Selection.resize(2);
@@ -25,6 +31,10 @@ std::vector<Laser> ReachedExitPoint(const Laser& LaserSet, float ExitBoundary) {
 }
 
 std::vector<Laser> SplitTrackSet(const Laser& LaserSet, unsigned int Downsample) {
+    /*
+     * This function separates the input laser data set into multiple smaller data sets,
+     * the number of produced set is specified by the Downsample value.
+     */
     std::vector<Laser> Sets;
     Sets.resize(Downsample);
 
@@ -32,9 +42,8 @@ std::vector<Laser> SplitTrackSet(const Laser& LaserSet, unsigned int Downsample)
     {
 
         auto SourceTrack = Track.GetReco();
-        unsigned long TrackSize = Track.GetNumberOfSamples();
 
-        for (unsigned long offset=0; offset < Sets.size(); offset++){
+        for (unsigned long offset=0; offset < Downsample; offset++){
             std::vector<ThreeVector<float>> SampledRecoTrack;
 
             for (unsigned long idx=offset; idx < SourceTrack.size(); idx += Downsample) {
